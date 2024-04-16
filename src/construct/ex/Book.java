ddbc8a148dbd456521585ae4ec5c78d3f80ba2a4
cheoljundev <cheoljundev@gmail.com>
2024-04-16 14:52:23 +0900

    생성자 완료

---
+++ src/construct/ex/Book.java
@@ -0,0 +1,25 @@
+package construct.ex;
+
+public class Book {
+    String name;
+    String author;
+    int pages;
+
+    Book(){
+        this("", "", 0);
+    }
+
+    Book(String name, String author) {
+        this(name, author, 0);
+    }
+
+    Book(String name, String author, int pages) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    void displayInfo(){
+        System.out.println("제목:" + name + ", 저자:" + author + ", 페이지:" + pages);
+    }
+}
