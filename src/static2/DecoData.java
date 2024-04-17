488c3054e8651a4fbdde7ae209d1b9a0a24b8b42
cheoljundev <cheoljundev@gmail.com>
2024-04-17 14:31:43 +0900

    static 메서드 완료

---
+++ src/static2/DecoData.java
@@ -0,0 +1,34 @@
+package static2;
+
+public class DecoData {
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+//        instanceValue++; // instance 변수 접근, 컴파일 에러
+//        instanceMethod(); // instance 메서드 접근, 컴파일 에러
+
+        staticValue++; // static 변수 접근
+        staticMethod(); // static 메서드 접근
+    }
+
+    public static void staticCall(DecoData data) {
+        data.instanceValue++;
+        data.instanceMethod();
+    }
+
+    public void instanceCall() {
+        instanceValue++; // instance 변수 접근
+        instanceMethod(); // instance 메서드 접근
+
+        staticValue++; // static 변수 접근
+        staticMethod(); // static 메서드 접근
+    }
+    private void instanceMethod() {
+        System.out.println("instanceValue = " + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue = " + staticValue);
+    }
+}
