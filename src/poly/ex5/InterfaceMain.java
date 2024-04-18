1149cf7fc577e7390add38965a666842fec22e9d
cheoljundev <cheoljundev@gmail.com>
2024-04-18 17:48:36 +0900

    인터페이스 개념

---
+++ src/poly/ex5/InterfaceMain.java
@@ -0,0 +1,23 @@
+package poly.ex5;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        // InterfaceAnimal interfaceAnimal = new InterfaceAnimal(); // 인스턴스 생성 불가
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(cow);
+
+    }
+
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 사운드 시작");
+        animal.sound();
+        System.out.println("동물 사운드 종료");
+    }
+}
