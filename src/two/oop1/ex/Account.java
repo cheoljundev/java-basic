5accb5c37477012ded0db100c214246267b6dd58
cheoljundev <cheoljundev@gmail.com>
2024-10-27 18:55:10 +0900

    객체 지향 프로그래밍 문제와 플이

---
+++ src/two/oop1/ex/Account.java
@@ -0,0 +1,15 @@
+package two.oop1.ex;
+
+public class Account {
+    int balance;
+    void deposit(int amount){
+        balance += amount;
+    }
+    void withdraw(int amount){
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("잔액이 부족합니다.");
+        }
+    }
+}
