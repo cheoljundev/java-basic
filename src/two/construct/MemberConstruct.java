250818d3e809700ba6119419d91b125f66ba1495
cheoljundev <cheoljundev@gmail.com>
2024-10-28 21:10:53 +0900

    생성자 오버로딩과 this()

--- src/two/construct/MemberConstruct.java
+++ src/two/construct/MemberConstruct.java
@@ -5,6 +5,16 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    // 추가
+
+
+    public MemberConstruct(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+        this(name, age, 50);
+    }
+
     public MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name = " + name + " age = " + age + " grade = " + grade);
         this.name = name;
