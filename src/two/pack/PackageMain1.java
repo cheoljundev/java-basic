18b42f429061df4fef55fe0891913befbb9c990c
cheoljundev <cheoljundev@gmail.com>
2024-11-01 22:25:08 +0900

    패키지 시작

---
+++ src/two/pack/PackageMain1.java
@@ -0,0 +1,8 @@
+package two.pack;
+
+public class PackageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        two.pack.a.User user = new two.pack.a.User();
+    }
+}
---
+++ src/two/pack/a/User.java
@@ -0,0 +1,7 @@
+package two.pack.a;
+
+public class User {
+    public User() {
+        System.out.println("패키지 Pack a 회원 생성");
+    }
+}
