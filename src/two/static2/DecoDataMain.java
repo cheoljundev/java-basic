9d98646b9174135d9220cd04641cafdb048cad2b
cheoljundev <cheoljundev@gmail.com>
2024-11-02 18:27:24 +0900

    static 메서드3

--- src/two/static2/DecoDataMain.java
+++ src/two/static2/DecoDataMain.java
@@ -1,8 +1,13 @@
 package two.static2;
 
+import static two.static2.DecoData.staticCall; // static import
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
+        DecoData.staticCall();
+        DecoData.staticCall();
+        DecoData.staticCall();
         DecoData.staticCall();
 
         System.out.println("2. 인스턴스 호출 1");
